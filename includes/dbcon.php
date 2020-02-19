411cc65c2ac292abb2fa30d522ee2efa074e051b
Jos33y <josephlagbalu@gmail.com>
2020-02-19 08:52:37 +0100

    added all files

---
+++ includes/dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+//$con = mysqli_connect("localhost", "Joseey", "", "ecom_store_doris");
+
+$con = mysqli_connect("localhost", "u721127448_Faithy", "LagbaluJ19", "u721127448_slaywithfaithy");
+
+//$conn = mysqli_connect("localhost", "Joseey", "", "eacez_db");
+
+$conn = mysqli_connect("localhost", "u721127448_Admin", "LagbaluJ19", "u721127448_eacez");
+
+?>
